54c2a58f01a201b8cd59876cfadd91de69e33dfb
Vaaragh <vaarghl@gmail.com>
2023-04-29 16:22:57 +0200

    feat: add main view, login view, guest view

---
+++ app/src/main/java/game/slagalica/GuestHomeActivity.java
@@ -0,0 +1,14 @@
+package game.slagalica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuestHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_home);
+    }
+}
--- app/src/main/java/game/slagalica/MainActivity.java
+++ app/src/main/java/game/slagalica/MainActivity.java
@@ -1,6 +1,9 @@
 package game.slagalica;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,24 +17,25 @@ import game.slagalica.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        Button guestBtn =(Button)findViewById(R.id.guest_btn);
+        guestBtn.setOnClickListener((view -> {
+            Intent intent =new Intent(MainActivity.this, GuestHomeActivity.class);
+            startActivity(intent);
+        }));
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        Button loginBtn = (Button) findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener((view -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }));
+
     }
 
 }

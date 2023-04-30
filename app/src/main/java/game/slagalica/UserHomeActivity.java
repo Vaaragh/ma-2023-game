cc46104dabd3c89510f46bba043a134f48800696
Vaaragh <vaarghl@gmail.com>
2023-04-30 19:59:15 +0200

    feat: add logout function

--- app/src/main/java/game/slagalica/UserHomeActivity.java
+++ app/src/main/java/game/slagalica/UserHomeActivity.java
@@ -41,5 +41,6 @@ public class UserHomeActivity extends AppCompatActivity {
 
 
 
+
     }
 }
--- app/src/main/java/game/slagalica/userFragments/ProfileFragment.java
+++ app/src/main/java/game/slagalica/userFragments/ProfileFragment.java
@@ -1,21 +1,38 @@
 package game.slagalica.userFragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import game.slagalica.MainActivity;
 import game.slagalica.R;
+import game.slagalica.UserHomeActivity;
+
 public class ProfileFragment extends Fragment {
 
 
 
-    public ProfileFragment() {
-        super(R.layout.fragment_profile);
+    public ProfileFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        Button logoutBtn = rootView.findViewById(R.id.logout_btn);
+        logoutBtn.setOnClickListener((view -> {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
+        }));
+        return rootView;
     }
+
+
 
 
 }
